15708888e7e886d1629f1de000517efd6e25d85a
leo <62425425+lleaob@users.noreply.github.com>
2026-05-27 16:31:21 +0100

    feat: configure database connection

--- src/VEGG.TABLE.API/GlobalUsings.cs
+++ src/VEGG.TABLE.API/GlobalUsings.cs
@@ -2,3 +2,6 @@ global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Mvc;
 
 global using VEGG.TABLE.Core.Interfaces;
+global using VEGG.TABLE.Infrastructure;
+global using VEGG.TABLE.Infrastructure.Data;
+global using Microsoft.EntityFrameworkCore;
--- src/VEGG.TABLE.API/Program.cs
+++ src/VEGG.TABLE.API/Program.cs
@@ -8,6 +8,11 @@ builder.Services.AddOpenApi();
 // Add other services
 builder.Services.AddControllers();
 
+// Get the connection string from appsettings.json
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Add a service to the build in the form of our database context, configured to use SQL Server.
+builder.Services.AddDbContext<DBContext>(options => options.UseSqlServer(connectionString));
+
 var app = builder.Build();
 
 // 2. Configure the HTTP request pipeline
