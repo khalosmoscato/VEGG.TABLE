0f57f5ddb0e147304e4d7945d17ecac38ac03264
leo <62425425+lleaob@users.noreply.github.com>
2026-05-28 12:34:37 +0100

    fix: remove unnecessary XUnit tests

--- tests/VEGG.TABLE.UnitTests/Services/ExternalServiceTests.cs
+++ tests/VEGG.TABLE.UnitTests/Services/ExternalServiceTests.cs
@@ -1,8 +1,1 @@
 ﻿namespace VEGG.TABLE.UnitTests.Services;
-
-public class ExternalServiceTests // Tests for the external API integration logic
-{
-    private readonly Mock<IHttpClientFactory> _httpClientFactoryMock = new();
-
-
-}
