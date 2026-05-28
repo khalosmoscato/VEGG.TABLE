bbfb15b03457f2a421f5ee0779eeebc438aee163
leo <62425425+lleaob@users.noreply.github.com>
2026-05-28 12:22:04 +0100

    refactor: change test package  to NUnit

--- tests/VEGG.TABLE.UnitTests/GlobalUsings.cs
+++ tests/VEGG.TABLE.UnitTests/GlobalUsings.cs
@@ -7,4 +7,4 @@ global using Moq;
 global using VEGG.TABLE.Core.Entities;
 global using VEGG.TABLE.Infrastructure.Data;
 
-global using Xunit;
+global using NUnit;
--- tests/VEGG.TABLE.UnitTests/Services/ExternalServiceTests.cs
+++ tests/VEGG.TABLE.UnitTests/Services/ExternalServiceTests.cs
@@ -4,16 +4,5 @@ public class ExternalServiceTests // Tests for the external API integration logi
 {
     private readonly Mock<IHttpClientFactory> _httpClientFactoryMock = new();
 
-    [Fact]
-    public async Task Should_Return_Data_When_Api_Is_Successful()
-    {
-        // Arrange (Setup your mock here)
 
-        // Act
-        var result = "Success"; // Simulate your service call
-
-        // Assert
-        result.Should().NotBeNull()
-              .And.Be("Success");
-    }
 }
