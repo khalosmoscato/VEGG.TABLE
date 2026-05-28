b1ddc678bd2141a9a10215b2676e12e4ec4ff6a2
leo <62425425+lleaob@users.noreply.github.com>
2026-05-28 12:34:37 +0100

    fix: remove unnecessary XUnit tests

--- tests/VEGG.TABLE.UnitTests/Domain/EntityNameTests.cs
+++ tests/VEGG.TABLE.UnitTests/Domain/EntityNameTests.cs
@@ -1,6 +1,1 @@
 namespace VEGG.TABLE.UnitTests.Domain;
-
-public class UserTests // Tests for User, Table, and Produce logic
-{
-
-}
--- tests/VEGG.TABLE.UnitTests/Repositories/ProduceRepositoryTests.cs
+++ tests/VEGG.TABLE.UnitTests/Repositories/ProduceRepositoryTests.cs
@@ -1,6 +1,1 @@
 ﻿namespace VEGG.TABLE.UnitTests.Repositories;
-
-public class ProduceRepositoryTests // Tests for the data access layer
-{
-
-}
