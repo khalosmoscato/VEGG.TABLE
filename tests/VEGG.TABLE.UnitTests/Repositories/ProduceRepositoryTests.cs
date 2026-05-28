9cd0b58870713f9e07ec071af9723b6156169f27
leo <62425425+lleaob@users.noreply.github.com>
2026-05-28 12:22:04 +0100

    refactor: change test package  to NUnit

--- tests/VEGG.TABLE.UnitTests/Repositories/ProduceRepositoryTests.cs
+++ tests/VEGG.TABLE.UnitTests/Repositories/ProduceRepositoryTests.cs
@@ -2,14 +2,5 @@
 
 public class ProduceRepositoryTests // Tests for the data access layer
 {
-    [Fact]
-    public void Should_Be_Empty_Initially()
-    {
-        // Arrange
-        var repository = new ProduceRepository();
 
-        // Act & Assert
-        // Assuming your repository has a way to check count/content
-        repository.Should().NotBeNull();
-    }
 }
