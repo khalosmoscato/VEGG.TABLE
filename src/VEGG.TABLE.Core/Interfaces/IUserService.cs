708b0431770c8f53e07bfe6e3a60b921f0d6a69a
Leoreoreoreo <jinluezhang@gmail.com>
2026-05-28 10:15:49 +0100

    feat: add IUserService interface

---
+++ src/VEGG.TABLE.Core/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+using VEGG.TABLE.Core.Entities;
+namespace VEGG.TABLE.Core.Interfaces;
+
+public interface IUserService
+{
+    List<User> GetAllUsers();
+    User GetUserById(int id);
+    User AddUser(User user);
+    User UpdateUser(int id, User user);
+    bool DeleteUser(int id);
+}
