70eae0c9eef2426f079ce70d34f555b0e4f6137f
leo <62425425+lleaob@users.noreply.github.com>
2026-05-29 11:27:11 +0100

    feat: create test file for farm repo layer

---
+++ tests/VEGG.TABLE.UnitTests/Repositories/FarmRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VEGG.TABLE.UnitTests.Repositories;
+
+public class FarmRepositoryTests
+{
+    [Test]
+    public void Test()
+    {
+
+    }
+}
