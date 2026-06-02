42a57f36d1e429d626b52a4213b4cb873b2aad0b
leo <62425425+lleaob@users.noreply.github.com>
2026-06-02 10:57:17 +0100

    feat: Add AuthController and login endpoint

--- src/VEGG.TABLE.API/Program.cs
+++ src/VEGG.TABLE.API/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IProduceRepository, ProduceRepository>();
 builder.Services.AddScoped<IProduceService, ProduceService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 // Get the connection string from appsettings.json
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
