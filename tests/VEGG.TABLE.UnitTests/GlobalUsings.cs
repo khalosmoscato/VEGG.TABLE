690bde9a7e4d50819b17acb1fade6bf7156bde47
leo <62425425+lleaob@users.noreply.github.com>
2026-06-02 12:25:53 +0100

    test: add Microsoft.EntityFrameworkCore.InMemory

--- tests/VEGG.TABLE.UnitTests/GlobalUsings.cs
+++ tests/VEGG.TABLE.UnitTests/GlobalUsings.cs
@@ -11,3 +11,5 @@ global using VEGG.TABLE.Core.Entities;
 global using VEGG.TABLE.Core.Interfaces;
 global using VEGG.TABLE.Infrastructure.Data;
 global using VEGG.TABLE.Infrastructure.Services;
+
+global using Microsoft.EntityFrameworkCore;
