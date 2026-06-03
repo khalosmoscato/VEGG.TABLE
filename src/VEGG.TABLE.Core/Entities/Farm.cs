c987f4b0a06dfc90c3dafb53feb49d3cb0aadf38
leo <62425425+lleaob@users.noreply.github.com>
2026-06-03 11:06:51 +0100

    feat: add annotations to the Farm class for database build

--- src/VEGG.TABLE.Core/Entities/Farm.cs
+++ src/VEGG.TABLE.Core/Entities/Farm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace VEGG.TABLE.Core.Entities;
@@ -7,7 +8,8 @@ namespace VEGG.TABLE.Core.Entities;
 public class Farm
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    [Required, StringLength(100)]
+    public required string Name { get; set; }
     public double Lat { get; set; }
     public double Lng { get; set; }
     public int OwnerId { get; set; }
