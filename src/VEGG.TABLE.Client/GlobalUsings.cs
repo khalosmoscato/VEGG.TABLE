80a1443ba53e8bd99b6d39736cec5fbbb5ab0a72
leo <62425425+lleaob@users.noreply.github.com>
2026-06-03 14:14:49 +0100

    feat: Implment map API and /map page

--- src/VEGG.TABLE.Client/GlobalUsings.cs
+++ src/VEGG.TABLE.Client/GlobalUsings.cs
@@ -5,3 +5,5 @@ global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.Forms;
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Http;
+
+global using VEGG.TABLE.Core.Entities;
---
+++ src/VEGG.TABLE.Client/wwwroot/js/mapHelper.js
@@ -0,0 +1,26 @@
+﻿export function initializeMap(elementId, farms) {
+    function tryInit() {
+        const container = document.getElementById(elementId);
+        if (!container) {
+            requestAnimationFrame(tryInit);
+            return;
+        }
+
+        var map = L.map(elementId).setView([51.5074, -0.1278], 12);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© OpenStreetMap contributors'
+        }).addTo(map);
+
+        farms.forEach(farm => {
+            L.marker([farm.lat, farm.lng])
+                .addTo(map)
+                .bindPopup(`<b>${farm.name}</b><br>Owner ID: ${farm.ownerId}`);
+        });
+        window.addEventListener('resize', () => {
+            map.invalidateSize();
+        });
+    }
+
+    tryInit();
+}
