bea074d9c3aa13bf4b7b1bdab4382b67d189bbe1
chloerb97 <chloe.rb@hotmail.co.uk>
2026-06-03 17:10:22 +0100

    Added comments

--- src/VEGG.TABLE.Client/wwwroot/js/mapHelper.js
+++ src/VEGG.TABLE.Client/wwwroot/js/mapHelper.js
@@ -14,16 +14,17 @@
             attribution: '© OpenStreetMap contributors'
         }).addTo(map);
 
+        // Processes each farm to create pins and list items
         farms.forEach(farm => {
-           
+            // Creates the Marker on the map
             const marker = L.marker([farm.lat, farm.lng])
                 .addTo(map)
                 .bindPopup(`<b>${farm.name}</b><br>Owner ID: ${farm.ownerId}`);
-
+            // Creates the Sidebar Item
             const item = document.createElement('div');
             item.className = "p-4 border-b cursor-pointer hover:bg-green-50 transition";
             item.innerHTML = `<h3 class="font-semibold text-green-800">${farm.name}</h3>`;
-
+            // Links the click event to the map interaction
             item.onclick = () => {
                 map.setView([farm.lat, farm.lng], 15); 
                 marker.openPopup(); 
