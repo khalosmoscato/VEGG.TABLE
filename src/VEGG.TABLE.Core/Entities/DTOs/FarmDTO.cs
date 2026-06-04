1a3a59229fa20a74d7fa2a977b33064a6fb57d03
leo <62425425+lleaob@users.noreply.github.com>
2026-06-04 11:17:21 +0100

    refactor: implement FarmDto for API responses

---
+++ src/VEGG.TABLE.Core/Entities/DTOs/FarmDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VEGG.TABLE.Core.Entities.DTOs;
+
+public class FarmDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Lat { get; set; }
+    public double Lng { get; set; }
+    public string OwnerName { get; set; } = string.Empty;
+}
