04cf669f0e1eff7891ce17bbf3fe605d80f308b7
VladStam <vlad.inbox1999@gmail.com>
2026-06-04 10:28:57 +0100

    API Produce testing and get all produce by userID (#140)
    * Add the get produce by user API call
    
    Closes #106
    
    * Add endpoint to get all produce from a user Id including what is not on sale
    
    * Add dummy Produce for testing
    
    * Add test to get all produce for a user profile
    
    * Add produce service test to check that you can get all the produce a user has for sale
    
    * Refactor the PRoduce controller tests to be more accurate and test for resultobjects
    
    * Refactored test to meet Kanban
    
    * Added Nocontyent vs not found functionality to the get produce by userID call
    
    * Checkout commit
    
    * Fixed failing test
    
    * PR feedback edits and addition of produce onSale tests
    
    Closes #108
    
    * Tests all pass
    
    * Add DTO to addProduce call and related tests

--- tests/VEGG.TABLE.UnitTests/ServicesTests/ProduceServiceTests.cs
+++ tests/VEGG.TABLE.UnitTests/ServicesTests/ProduceServiceTests.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
+
+using VEGG.TABLE.UnitTests.Resources;
+
 namespace VEGG.TABLE.UnitTests.Services;
 
 public class ProduceServiceTests
@@ -5,44 +9,95 @@ public class ProduceServiceTests
     private Mock<IProduceRepository> _mockRepo = null!;
     private ProduceService _service = null!;
 
+    private static List<Produce> testProduce = new List<Produce> { };
+
     [SetUp]
     public void Setup()
     {
         _mockRepo = new Mock<IProduceRepository>();
         _service = new ProduceService(_mockRepo.Object);
+
+        testProduce = DummyProduce.DummyProduceList;
     }
 
     [Test]
     public void GetAllProduces_ReturnsAllFromRepository()
     {
-        var produces = new List<Produce>
-        {
-            new Produce { ProduceId = 1, Name = "Apples", UserId = 1 },
-            new Produce { ProduceId = 2, Name = "Bananas", UserId = 2 }
-        };
-        _mockRepo.Setup(r => r.GetAllProduces()).Returns(produces);
-
+        //ARRANGE
+        var produces = testProduce;
+        _mockRepo.Setup(r => r.GetAllProduces()).Returns(testProduce);
+        //ACT
         var result = _service.GetAllProduces();
+        //ASSERT
+        result.Should().BeEquivalentTo(testProduce);
+    }
 
-        result.Should().BeEquivalentTo(produces);
+    [Test]
+    public void GetAllProducesOnSale_ReturnsAllOnSaleProduce()
+    {
+        //arrange
+        List<Produce> produces = testProduce;
+        List<Produce>? expectedProduces = produces.Where(p => p.IsOnSale == true).ToList();
+        if (expectedProduces.Count == 0)
+        {
+            expectedProduces = null;
+        }
+        _mockRepo.Setup(r => r.GetAllProduceOnSale()).Returns(expectedProduces);
+
+        //Act
+        List<Produce>? result = _service.GetAllProduceOnSale();
+
+        //ASSERT
+        //check that the correct function is called
+        _mockRepo.Verify(x => x.GetAllProduceOnSale(), Times.Once);
+        //check result type
+        Assert.IsInstanceOf<List<Produce>>(result);
+        //check the data is matching expected
+        Assert.IsNotNull(result);
+        Assert.That(result, Is.EquivalentTo(expectedProduces));
+    }
+
+    [Test]
+    public void GetAllProducesOnSale_ReturnsEmptyProduceWhenNothingOnSale()
+    {
+        //arrange
+        List<Produce> produces = new List<Produce>(); ;
+        List<Produce>? expectedProduces = produces.Where(p => p.IsOnSale == true).ToList();
+        if (expectedProduces.Count == 0)
+        {
+            expectedProduces = null;
+        }
+        _mockRepo.Setup(r => r.GetAllProduceOnSale()).Returns(expectedProduces);
+
+        //Act
+        var result = _service.GetAllProduceOnSale();
+
+        //ASSERT
+        //check that the correct function is called
+        _mockRepo.Verify(x => x.GetAllProduceOnSale(), Times.Once);
+        //check result type
+        Assert.IsNull(result);
+
     }
 
     [Test]
     public void GetProduceById_ReturnsProduce_WhenFound()
     {
+        //ARRANGE
         var produce = new Produce { ProduceId = 1, Name = "Apples", UserId = 1 };
         _mockRepo.Setup(r => r.GetProduceById(1)).Returns(produce);
-
+        //ACT
         var result = _service.GetProduceById(1);
-
+        //ASSERT
         result.Should().Be(produce);
     }
 
     [Test]
     public void GetProduceById_ReturnsNull_WhenNotFound()
     {
+        //ARRANGE
         _mockRepo.Setup(r => r.GetProduceById(99)).Returns((Produce?)null);
-
+        //ACT
         var result = _service.GetProduceById(99);
 
         result.Should().BeNull();
@@ -51,32 +106,105 @@ public class ProduceServiceTests
     [Test]
     public void AddProduce_ReturnsCreatedProduce()
     {
-        var produce = new Produce { ProduceId = 1, Name = "Apples", UserId = 1 };
-        _mockRepo.Setup(r => r.AddProduce(produce)).Returns(produce);
-
-        var result = _service.AddProduce(produce);
-
+        //ARRANGE
+        var produceList = testProduce;
+        var produceDTO = new ProduceDTO {  Name = "Apples", UserId = 1 };
+        var produce = new Produce { ProduceId = 8, Name = "Apples", UserId = 1 };
+        _mockRepo.Setup(r => r.AddProduce(produceDTO)).Returns(produce);
+        //ACT
+        var result = _service.AddProduce(produceDTO);
+        //ASSERT
         result.Should().Be(produce);
-        _mockRepo.Verify(r => r.AddProduce(produce), Times.Once);
+        _mockRepo.Verify(r => r.AddProduce(produceDTO), Times.Once);
     }
 
     [Test]
     public void DeleteProduce_ReturnsTrue_WhenDeleted()
     {
+        //ARRANGE
         _mockRepo.Setup(r => r.DeleteProduce(1)).Returns(true);
-
+        //ACT
         var result = _service.DeleteProduce(1);
-
+        //ASSERT
         result.Should().BeTrue();
     }
 
     [Test]
     public void DeleteProduce_ReturnsFalse_WhenNotFound()
     {
+        //ARRANGE
         _mockRepo.Setup(r => r.DeleteProduce(99)).Returns(false);
-
+        //ACT
         var result = _service.DeleteProduce(99);
-
+        //ASSERT
         result.Should().BeFalse();
+    }
+
+    [Test]
+    public void GetProduceByUserIdAll_returnsProduce()
+    {
+        //Arrange
+        var parameter = 1;
+        var produceList = DummyProduce.DummyProduceList;
+        var expectedProduce = produceList.Where(p => p.UserId == parameter).ToList();
+        _mockRepo.Setup(r => r.GetProduceByUserIdAll(parameter)).Returns(expectedProduce);
+        
+        foreach(Produce p in expectedProduce) Console.WriteLine(p.Name + p.ProduceId);
+        //Act
+        var result = _service.GetProduceByUserIdAll(parameter);
+
+        //ASSERT
+        //check that the correct function is called
+        _mockRepo.Verify(x => x.GetProduceByUserIdAll(parameter), Times.Once);
+        //check result type
+        Assert.IsInstanceOf<List<Produce>>(result);
+        //check the data is matching expected
+        Assert.IsNotNull(result);
+        Assert.That(result, Is.EquivalentTo(expectedProduce));
+    }
+
+    [Test]
+    public void GetProduceByUserIdAll_returnsNotFound()
+    {
+        //Arrange
+        var parameter = 0;
+        var produceList = DummyProduce.DummyProduceList;
+        var expectedProduce = produceList.Where(p => p.UserId == parameter).ToList();
+        _mockRepo.Setup(r => r.GetProduceByUserIdAll(parameter)).Returns(expectedProduce);
+
+        foreach (Produce p in expectedProduce) Console.WriteLine(p.Name + p.ProduceId);
+        //Act
+        var result = _service.GetProduceByUserIdAll(parameter);
+
+        //ASSERT
+        //check that the correct function is called
+        _mockRepo.Verify(x => x.GetProduceByUserIdAll(parameter), Times.Once);
+        //check result type
+        Assert.IsInstanceOf<List<Produce>>(result);
+        //check the data is matching expected
+        Assert.That(result, Is.EquivalentTo(expectedProduce));
+    }
+
+    [Test]
+    public void GetProduceByUserId_returnsProduceOnSale()
+    {
+        //ARRANGE
+        var parameter = 1;
+        var produceList = DummyProduce.DummyProduceList;
+        var expectedProduce = produceList.Where(p => p.UserId == parameter && p.IsOnSale == true).ToList();
+        _mockRepo.Setup(r => r.GetProduceByUserId(parameter)).Returns(expectedProduce);
+
+        foreach (Produce p in expectedProduce) Console.WriteLine(p.Name + p.ProduceId);
+        //ACT
+        var result = _service.GetProduceByUserId(parameter);
+
+        //ASSERT
+        //check that the correct function is called
+        _mockRepo.Verify(x => x.GetProduceByUserId(parameter), Times.Once);
+        //check result type
+        Assert.IsInstanceOf<List<Produce>>(result);
+        //check the data is matching expected
+        Assert.IsNotNull(result);
+        Assert.That(result, Is.EquivalentTo(expectedProduce));
     }
 }
